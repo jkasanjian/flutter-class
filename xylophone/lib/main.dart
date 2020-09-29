b5501a04b1e705f38205995431af49004b809400
Joshua Kasanjian <jkasanjian@gmail.com>
2020-09-28 18:26:33 -0700

    testing packages

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,7 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(child: Text('test')),
         ),
       ),
     );
