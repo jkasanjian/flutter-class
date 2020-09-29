d41f9c0488f4d2a0f0f5f156a2346da46b4028a0
Joshua Kasanjian <jkasanjian@gmail.com>
2020-09-29 00:14:49 -0700

    works using two lists

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -25,6 +25,49 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> answers = [false, true, true];
+
+  int qNum = 0;
+
+  void updateIdx() {
+    setState(() {
+      qNum++;
+      if (qNum >= questions.length) {
+        qNum = 0;
+      }
+    });
+  }
+
+  void addCheck() {
+    setState(() {
+      scoreKeeper.add(
+        Icon(
+          Icons.check,
+          color: Colors.green,
+        ),
+      );
+    });
+  }
+
+  void addX() {
+    setState(() {
+      scoreKeeper.add(
+        Icon(
+          Icons.close,
+          color: Colors.red,
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +80,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[qNum],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +105,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                if (answers[qNum]) {
+                  addCheck();
+                } else {
+                  addX();
+                }
+                updateIdx();
               },
             ),
           ),
@@ -80,11 +129,19 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                if (!(answers[qNum])) {
+                  addCheck();
+                } else {
+                  addX();
+                }
+                updateIdx();
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
