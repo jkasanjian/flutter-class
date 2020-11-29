7705fc8cf004118818dada654dbeea6e2288ac12
Joshua Kasanjian <jkasanjian@gmail.com>
2020-11-28 23:14:42 -0800

    finished challenge level silver

--- bitcoin-ticker/lib/price_screen.dart
+++ bitcoin-ticker/lib/price_screen.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'coin_data.dart';
+import 'network.dart';
+import 'dart:io' show Platform;
+
+const BASE_URL = 'https://rest.coinapi.io/v1/exchangerate';
+const API_KEY = '35FC80C5-1D66-4250-9DF7-E5F0A963CBD2';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,11 +14,86 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCur = 'USD';
+  double btc;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void updateUI(String val) async {
+    dynamic coinData = await getValues(val);
+    setState(
+      () {
+        if (coinData == null) {
+          btc = 0.00;
+          selectedCur = 'ERROR';
+        }
+        selectedCur = val;
+        btc = coinData['rate'];
+      },
+    );
+  }
+
+  Future<dynamic> getValues(String curr) async {
+    NetworkHelper networkHelper =
+        NetworkHelper('$BASE_URL/BTC/$curr?apikey=$API_KEY');
+    var data = await networkHelper.getData();
+    return data;
+  }
+
+  DropdownButton<String> androidDropdown() {
+    List<DropdownMenuItem<String>> dropItems = [];
+    for (String s in currenciesList) {
+      dropItems.add(DropdownMenuItem<String>(child: Text(s), value: s));
+    }
+    return DropdownButton<String>(
+      value: selectedCur,
+      items: dropItems,
+      onChanged: (value) {
+        setState(
+          () {
+            selectedCur = value;
+            updateUI(value);
+          },
+        );
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+    List<Text> pickerItems = [];
+    for (String s in currenciesList) {
+      pickerItems.add(Text(
+        s,
+        style: TextStyle(color: Colors.white),
+      ));
+    }
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        updateUI(currenciesList[selectedIndex]);
+      },
+      children: pickerItems,
+    );
+  }
+
+  String getText() {
+    if (btc != null) {
+      return '1 BTC = ${btc.toStringAsFixed(2)} $selectedCur';
+    } else {
+      return '1 BTC = ? $selectedCur';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        centerTitle: true,
+        title: Text('💱 Coin Ticker'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -27,7 +110,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  getText(),
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +125,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
