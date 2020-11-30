e44bdbb53df07af5fd7d60970c1b5aae51f62d4a
Joshua Kasanjian <jkasanjian@gmail.com>
2020-11-29 18:33:45 -0800

    renamed routes

--- flash-chat/lib/main.dart
+++ flash-chat/lib/main.dart
@@ -15,12 +15,12 @@ class FlashChat extends StatelessWidget {
           body1: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: '/',
+      initialRoute: 'welcome_screen',
       routes: {
-        '/': (context) => WelcomeScreen(),
-        '/login': (context) => LoginScreen(),
-        '/register': (context) => RegistrationScreen(),
-        '/chat': (context) => ChatScreen(),
+        'welcome_screen': (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'register_screen)': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen(),
       },
     );
   }
