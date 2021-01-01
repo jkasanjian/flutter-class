7ae329a6ba4083ed35afeb5e1a30a706698672d8
Joshua Kasanjian <jkasanjian@gmail.com>
2020-12-31 19:36:03 -0800

    added routes with constant ids for each page

--- flash-chat/lib/screens/welcome_screen.dart
+++ flash-chat/lib/screens/welcome_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
@@ -44,7 +46,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 borderRadius: BorderRadius.circular(30.0),
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id)
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -62,7 +64,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 elevation: 5.0,
                 child: MaterialButton(
                   onPressed: () {
-                    //Go to registration screen.
+                   Navigator.pushNamed(context, RegistrationScreen.id)
                   },
                   minWidth: 200.0,
                   height: 42.0,
